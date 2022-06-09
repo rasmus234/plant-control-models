9859a6a27e3942cfc969633d8d4ab0889d1c0c3c
Rasmus <66802136+rasmus234@users.noreply.github.com>
2022-06-09 19:46:26 +0200

    make logintime optional

--- PlantControl.Models/Logger.cs
+++ PlantControl.Models/Logger.cs
@@ -8,6 +8,6 @@ public class Logger
     [JsonPropertyName("_id")]
     public string Id { get; set; }
     public string Name { get; set; }
-    public DateTime LoginTime { get; set; }
+    public DateTime? LoginTime { get; set; }
     public bool IsPaired { get; set; } = false;
 }
