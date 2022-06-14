fa9fe9e6a7af6f5c423dc6e419b536a9e68adb85
Kristoffer-ProgGod <Kristoffer.frederichsen@gmail.com>
2022-06-14 14:13:38 +0200

    Added warning and ignore .suo file

---
+++ Warning.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace PlantControl.Models
+{
+    public class Warning
+    {
+        [JsonPropertyName("title")] public string Title { get; set; }
+        [JsonPropertyName("description")] public string Description { get; set; }
+        [JsonPropertyName("createdAt")] public DateTime CreatedAt { get; set; }
+    }
+}
